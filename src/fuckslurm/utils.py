30733c93c13ea4997373a4a6a13d4af0e1f9626c
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-06-21 20:55:38 -0400

    update

--- src/fuckslurm/utils.py
+++ src/fuckslurm/utils.py
@@ -5,25 +5,24 @@ import json
 
 
 def sbatch(job_command: str):
-    p = subprocess.Popen(["sbatch", *job_command.split(" ")])
-    if p.wait() != 0:
-        outputs = ""
+    p = subprocess.run(["sbatch", *job_command.split(" ")], capture_output=True)
+    if p.returncode != 0:
+        raise ValueError(p.stderr)
+    else:
         if p.stdout is not None:
-            outputs = p.stdout.read().decode("utf-8")
-        raise ValueError(
-            f"sbatch failed. Please check your job command. \n output: {outputs}"
-        )
+            stdout = p.stdout.decode("utf-8").strip()
+            print(stdout)
+            return stdout
 
 
 def scancel(job_id: int):
-    subprocess.Popen(["scancel", str(job_id)])
+    subprocess.run(["scancel", str(job_id)])
 
 
 def get_job_info_by_job_name(job_name: str) -> dict | None:
-    p = subprocess.Popen(["sacct", "-n", "--json", "-j", job_name])
-    p.wait()
-    if p.stdout is not None:
-        stdout = p.stdout.read().decode("utf-8").strip()
+    p = subprocess.run(["sacct", "-n", "--json", "-j", job_name])
+    if p.stdout is not None and p.stdout != b"":
+        stdout = p.stdout.decode("utf-8").strip()
         if len(stdout) == 0:
             return None
         info = json.loads(stdout)
@@ -32,10 +31,10 @@ def get_job_info_by_job_name(job_name: str) -> dict | None:
 
 
 def get_job_info_by_job_id(job_id: int) -> dict | None:
-    p = subprocess.Popen(["sacct", "-n", "--json", "-j", str(job_id)])
-    p.wait()
-    if p.stdout is not None:
-        stdout = p.stdout.read().decode("utf-8").strip()
+    p = subprocess.run(["sacct", "-n", "--json", "-j", str(job_id)])
+
+    if p.stdout is not None and p.stdout != b"":
+        stdout = p.stdout.decode("utf-8").strip()
         if len(stdout) == 0:
             return None
         info = json.loads(stdout)
